af075da765bd5efe7098e019dfe90ae196114e18
jinpx <hujb1983@gmail.com>
2018-11-12 19:55:55 +0800

    h

--- include/Define.h
+++ include/Define.h
@@ -3,21 +3,39 @@
 
 /////////////////////////////////////////////////////////////////////////////////
 // 类型定义
-typedef int					BOOL;
-typedef uint8_t				BYTE;
-typedef char				CHAR;
-typedef char *				LPCSTR;
-typedef unsigned int        UINT;
-typedef unsigned int        DWORD;
-typedef int					SOCKET;
+typedef int64_t				INT64;
+typedef int32_t				INT32;
+typedef int16_t				INT16;
+typedef int8_t				INT8;
+typedef uint64_t			UINT64;
 typedef uint32_t			UINT32;
+typedef uint16_t			UINT16;
+typedef uint8_t				UINT8;
+typedef int					SOCKET;
+typedef int					BOOL;
+
+typedef uint8_t				BYTE;
 typedef uint16_t			WORD;
 typedef uint32_t			DWORD;
 typedef uint16_t			USHORT;
 
+typedef float               FLOAT;
+typedef double				DOUBLE;
+typedef int                 INT;
+typedef unsigned int        UINT;
+typedef char				CHAR;
+typedef short				SHORT;
+typedef unsigned short		USHORT;
+typedef char				*LPSTR, *PSTR;
+typedef const char			*LPCSTR, *PCSTR;
+
 /////////////////////////////////////////////////////////////////////////////////
 typedef struct				sockaddr_in SOCKADDR_IN;
 typedef struct				sockaddr SOCKADDR;
+
+/////////////////////////////////////////////////////////////////////////////////
+#define INVALID_SOCKET		-1
+#define	SOCKET_ERROR		-1
 
 /////////////////////////////////////////////////////////////////////////////////
 #define FALSE				0
